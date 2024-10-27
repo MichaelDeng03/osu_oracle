d0305b237570053ef27cb06b56dd232c7767ff05
Michael Deng <michaeldeng2003@gmail.com>
2024-10-27 18:35:31 -0500

    Add get_beatmap(beatmap_id)

--- data_collection/__init__.py
+++ data_collection/__init__.py
@@ -1,5 +1,5 @@
-from .get_beatmaps import get_beatmaps
+from .get_beatmaps import get_beatmap
 from .get_user_scores import get_user_scores
 from .get_users import get_users
 
-__all__ = ['get_users', 'get_user_scores', "get_beatmaps"]
+__all__ = ['get_users', 'get_user_scores', "get_beatmap"]
--- data_collection/get_beatmaps.py
+++ data_collection/get_beatmaps.py
@@ -1,2 +1,27 @@
-def get_beatmaps():
-    pass
+from db import crud
+from db.orm import Session, models
+
+from .client import ossapi_client
+
+
+def get_beatmap(beatmap_id: int):
+    """
+    Gets a beatmap
+    """
+    beatmap = ossapi_client.beatmap(beatmap_id)
+    beatmap_data = {
+        "id": beatmap.id,
+        "mode": beatmap.mode.value,
+        "beatmapset_id": beatmap.beatmapset_id,
+        "difficulty_rating": beatmap.difficulty_rating,
+        "bpm": beatmap.bpm,
+        "cs": beatmap.cs,
+        "od": beatmap.difficulty_rating,
+        "ar": beatmap.ar,
+        "length": beatmap.total_length,
+        "version": beatmap.version,
+    }
+
+    with Session() as session:
+        beatmap = models.Beatmap(**beatmap_data)
+        crud.create_or_ignore(session, beatmap)
