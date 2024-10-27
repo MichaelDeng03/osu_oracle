e31af82a5f73642deacde2531ad4cb36cd72e8ac
Michael Deng <michaeldeng2003@gmail.com>
2024-10-27 17:50:07 -0500

    Add get_beatmaps skeleton

--- data_collection/__init__.py
+++ data_collection/__init__.py
@@ -1,16 +1,5 @@
-from dotenv import dotenv_values
-from ossapi import Ossapi
-
+from .get_beatmaps import get_beatmaps
 from .get_users import get_users
 from .get_users_scores import get_user_scores
-
-config = dotenv_values()
-client_id = config.get('osu_client_id')
-client_secret = config.get('osu_client_secret')
-
-if not client_id or not client_secret:
-    raise ValueError('osu! API credentials not found in .env file')
-
-ossapi_client = Ossapi(client_id, client_secret)
 
 __all__ = ['ossapi_client', 'get_users', 'get_user_scores']
