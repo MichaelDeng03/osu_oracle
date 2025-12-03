8361f4dbb541110e74b6e41f1ddac0250f872d9f
Michael Deng <michaeldeng2003@gmail.com>
2025-12-03 17:40:27 -0600

    Add fastapi + initial main.py

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
